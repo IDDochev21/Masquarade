94d2bbd50e28deabc15e956a5901d9e3d7d3ad42
MPNedelchev22 <iddochev21@codingburgas.bg>
2024-01-24 22:39:03 +0200

    Add supabase info

--- python/masquarade/masquarade/settings.py
+++ python/masquarade/masquarade/settings.py
@@ -72,12 +72,14 @@ WSGI_APPLICATION = 'masquarade.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres.ptiowvafquyfrlgublpv",
+        'PASSWORD': "d4v5R9yGe3foCVcp",
+        'HOST': "aws-0-eu-central-1.pooler.supabase.com",
+        'PORT': "6543", 
     }
 }
 
